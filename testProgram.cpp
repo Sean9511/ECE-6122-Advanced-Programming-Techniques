28395cdada0c3d86567078af5f515e6f477937f2
Sean <43709384+Sean9511@users.noreply.github.com>
2020-09-16 23:51:18 -0400

    Lab0

---
+++ testProgram.cpp
@@ -0,0 +1,22 @@
+/*
+ Authour: Shancong Mou
+ Class: ECE 6122
+ Last Date Modified: 08/26/2020
+ 
+ Description:
+ 
+ I, Shancong Mou, have successfully accessed my pace-ice account. I am able to transfer files from my pace-ice account to my local machine using an ftp client. I am ready for the next assignment on pace-ice.
+ */
+
+# include <iostream>
+using namespace std;
+int main()
+{
+    cout << "My name is: Shancong Mou"
+    "\nThis (\') is a single quote."
+    "\nThis (\") is a double quote."
+    "\nThis (/) is a forward slash."
+    "\nThis (\\) is a backslash."
+    << endl;
+    return 0;
+}
